a3c86eb8f8e5b3f87148ce1362480b29a9181a78
hdzierz <helge.dzierzon@plantandfood.co.nz>
2016-03-04 11:21:52 +1300

    Setup.py had wrong package name

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(name='Kaka API',
       version='0.1',
-      description='Python API to Kaka',
+      description='Python API for Kaka',
       author='Helge Dzierzon',
       author_email='helge.dzierzon@plantandfood.co.nz',
       url='',
-      packages=['Kaka'],
+      packages=['PyKaka'],
      )
 
 
